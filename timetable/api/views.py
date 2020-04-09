682e264f27604e7dc4b55f614f67b66374e059bc
Anton Abramov <antonabramov2000@gmail.com>
2020-04-09 21:22:53 +0700

    Наброски вьюх для всех моделей

--- timetable/api/views.py
+++ timetable/api/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+import api.models as models
+import api.serializers as serializers
 
-# Create your views here.
+
+class ElderView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Elder.objects.all()
+        serializer = serializers.ElderSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class GroupView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Group.objects.all()
+        serializer = serializers.GroupSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class SubjectView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Subject.objects.all()
+        serializer = serializers.SubjectSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class CabinetView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Cabinet.objects.all()
+        serializer = serializers.CabinetSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class TeacherView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Teacher.objects.all()
+        serializer = serializers.TeacherSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class EventView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Event.objects.all()
+        serializer = serializers.EventSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class ConsultationView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Consultation.objects.all()
+        serializer = serializers.ConsultationSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class SessionView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Session.objects.all()
+        serializer = serializers.SessionSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class TimetableView(viewsets.ViewSet):
+    def all(self, request):
+        queryset = models.Timetable.objects.all()
+        serializer = serializers.TimetableSerializers(queryset, many=True)
+        return Response(serializer.data)
