533d7ac290c4bdd639f8be00b549386c2e4996b0
Anton Abramov <antonabramov2000@gmail.com>
2020-04-09 15:54:44 +0700

    Добавил модель старосты

--- timetable/api/models.py
+++ timetable/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Elder(models.Model):
+    name = models.TextField()
+    phone = models.CharField(max_length=12)
+    mail = models.EmailField()
