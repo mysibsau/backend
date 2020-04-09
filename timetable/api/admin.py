f9544eb3a154c6ef9ebe236cd9f759ffdc9acf5d
Anton Abramov <antonabramov2000@gmail.com>
2020-04-09 17:43:01 +0700

    Зарегистрировал созданные модели

--- timetable/api/admin.py
+++ timetable/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+import api.models as models
+
+admin.site.register(models.Elder)
+admin.site.register(models.Group)
+admin.site.register(models.Subject)
+admin.site.register(models.Cabinet)
+admin.site.register(models.Teacher)
+admin.site.register(models.Consultation)
+admin.site.register(models.Session)
+admin.site.register(models.Timetable)
