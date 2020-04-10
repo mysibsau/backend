72bcc41847ad0c4d4b52465fdff448b65ee358a9
Anton Abramov <antonabramov2000@gmail.com>
2020-04-10 16:09:54 +0700

    Добавил потоки в парсер

--- parsers/groups.py
+++ parsers/groups.py
@@ -1,14 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from threading import Thread
 
 f = open('groups.csv', 'a')
 
-for i in range(0, 20000):
-    r = requests.get(
-        f'https://timetable.pallada.sibsau.ru/timetable/group/{i}')
-    soup = BeautifulSoup(r.text, "html.parser")
-    group = str(soup.find('h3', {'class': 'text-center bold'})).split('"')
-    if len(group) >= 3:
-        f.write("%d;%s\n" % (i, group[3]))
-        print("Нашел", i)
-f.close()
+
+def parse(num):
+    for i in range(num * 100, (num + 1) * 100):
+        r = requests.get(
+            f'https://timetable.pallada.sibsau.ru/timetable/group/{i}')
+        soup = BeautifulSoup(r.text, "html.parser")
+        group = str(soup.find('h3', {'class': 'text-center bold'})).split('"')
+        if len(group) >= 3:
+            f = open('groups.csv', 'a')
+            f.write(f'{i};{group[3]}\n')
+            print(f'Нашел {group[3]}, id:{i}')
+            f.close()
+
+
+for i in range(0, 100):
+    thread = Thread(target=parse, args=(i, ))
+    thread.start()
+
