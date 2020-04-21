5a67ae732264938ce5c31598900cf4da6560ab60
Anton Abramov <antonabramov2000@gmail.com>
2020-04-21 20:13:58 +0700

    Удалил лишний каталог

--- timetable/settings.py
+++ timetable/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'timetable',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
