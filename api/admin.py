71f8511c82cd922ff7c4aa0f30a15c37e94f4246
w0rng <antonabramov2000@gmail.com>
2020-05-20 23:01:27 +0700

    Добавил сортировку по группам в админку

--- api/admin.py
+++ api/admin.py
@@ -6,9 +6,6 @@ admin.site.register(models.Elder)
 admin.site.register(models.Group)
 admin.site.register(models.Subject)
 admin.site.register(models.Teacher)
-admin.site.register(models.Consultation)
-admin.site.register(models.Session)
-admin.site.register(models.Event)
 
 
 @admin.register(models.Cabinet)
@@ -16,14 +13,18 @@ class CabinetAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
 
 
-@admin.register(models.Day)
+@admin.register(models.TimetableGroup)
 class DayAdmin(admin.ModelAdmin):
-    list_display = ('group', 'day')
+    list_filter = ('group',)
+    list_display = ('group', 'day', 'even_week')
 
 
 @admin.register(models.Subgroup)
 class SubgroupAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'teacher', 'cabinet', 'subgroup')
+    list_display = ('subject', 'get_teachers', 'cabinet', 'subgroup')
+
+    def get_teachers(self, obj):
+        return ", ".join([s.name for s in obj.teacher.all()])
 
 
 @admin.register(models.Lesson)
