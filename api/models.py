3a0a6d1824077efec9f1786472fa9c08a020b7cd
w0rng <antonabramov2000@gmail.com>
2020-05-21 12:52:13 +0700

    Дни недели с 0 Удалил старост Переименовал всякие штуки Добавил расписание кабиентов и преподов

--- api/models.py
+++ api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 WEEKDAY = (
-    (1, 'Понедельник'),
-    (2, 'Вторник'),
-    (3, 'Среда'),
-    (4, 'Четверг'),
-    (5, 'Пятница'),
-    (6, 'Суббота'),
-    (7, 'Воскресенье'),
+    (0, 'Понедельник'),
+    (1, 'Вторник'),
+    (2, 'Среда'),
+    (3, 'Четверг'),
+    (4, 'Пятница'),
+    (5, 'Суббота'),
+    (6, 'Воскресенье'),
 )
 
 TYPES = (
@@ -29,28 +29,14 @@ class Person(models.Model):
         abstract = True
 
 
-class Elder(Person):
-    class Meta:
-        verbose_name = u'Староста'
-        verbose_name_plural = u'Старосты'
-
-
 class Group(Person):
     phone = None
-    elder = models.OneToOneField(
-        Elder,
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-        verbose_name='староста'
-        )
-
     class Meta:
         verbose_name = u'Группа'
         verbose_name_plural = u'Группы'
 
 
-class Teacher(Person):
+class Professor(Person):
     department = models.TextField(blank=True, verbose_name='кафедра')
 
     class Meta:
@@ -58,7 +44,7 @@ class Teacher(Person):
         verbose_name_plural = u'Преподаватели'
 
 
-class Cabinet(models.Model):
+class Place(models.Model):
     title = models.CharField(max_length=10, verbose_name='название')
 
     def __str__(self):
@@ -71,7 +57,7 @@ class Cabinet(models.Model):
 
 class Subject(models.Model):
     title = models.TextField(verbose_name='название')
-    view = models.PositiveSmallIntegerField(choices=TYPES, verbose_name='тип')
+    type = models.PositiveSmallIntegerField(choices=TYPES, verbose_name='тип')
 
     def __str__(self):
         return self.title
@@ -82,20 +68,17 @@ class Subject(models.Model):
 
 
 class Subgroup(models.Model):
-    teacher = models.ManyToManyField(Teacher, verbose_name='преподаватель')
-    cabinet = models.ForeignKey(
-        Cabinet, on_delete=models.CASCADE, verbose_name='аудитория')
-    subject = models.ForeignKey(
-        Subject, on_delete=models.CASCADE, verbose_name='предмет')
-    group = models.ForeignKey(
-        Group, on_delete=models.CASCADE, verbose_name='группа')
-    subgroup = models.IntegerField(default=0, verbose_name='подгруппа')
+    professors = models.ManyToManyField(Professor, verbose_name='преподаватель')
+    groups = models.ManyToManyField(Group, verbose_name='группа')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='аудитория')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='предмет')
+    num = models.IntegerField(default=0, verbose_name='подгруппа')
 
     def __str__(self):
         return str(self.subject)
 
     class Meta:
-        ordering = ['subgroup']
+        ordering = ['num']
         verbose_name = u'Подгруппа'
         verbose_name_plural = u'Подгруппы'
 
@@ -130,11 +113,21 @@ class TimetableGroup(Timetable):
         verbose_name_plural = u'Рассписание группы'
 
 
-class TimetableCabinet(Timetable):
-    cabinet = models.ForeignKey(
-        Cabinet, on_delete=models.CASCADE, verbose_name='кабинет')
+class TimetablePlace(Timetable):
+    place = models.ForeignKey(
+        Place, on_delete=models.CASCADE, verbose_name='кабинет')
 
     class Meta:
         ordering = ['day']
         verbose_name = u'Рассписание кабинета'
         verbose_name_plural = u'Рассписание кабинета'
+
+
+class TimetableProfessor(Timetable):
+    professor = models.ForeignKey(
+        Professor, on_delete=models.CASCADE, verbose_name='Преподаватель')
+
+    class Meta:
+        ordering = ['day']
+        verbose_name = u'Рассписание преподавателя'
+        verbose_name_plural = u'Рассписание преподавателя'
