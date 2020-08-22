00136f1b55be50d9cdd94f1869ce948ce5aecda7
w0rng <antonabramov2000@gmail.com>
2020-08-22 16:54:44 +0700

    Удалил лишие модели, добавил копку импорта

--- api/admin.py
+++ api/admin.py
@@ -1,50 +1,42 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.conf.urls import url
+from django.urls import path
 
 import api.models as models
+from api.services import setters
 
-admin.site.register(models.Group)
-admin.site.register(models.Subject)
-admin.site.register(models.Professor)
+from multiprocessing import Process
 
 
 @admin.register(models.TimetableGroup)
 class TimetableGroupAdmin(admin.ModelAdmin):
     list_filter = ('group',)
     list_display = ('group', 'day', 'even_week')
-    
-
-@admin.register(models.TimetablePlace)
-class TimetablePlaceAdmin(admin.ModelAdmin):
-    list_filter = ('place',)
-    list_display = ('place', 'day', 'even_week')
 
 
-@admin.register(models.TimetableProfessor)
-class TimetableProfessorAdmin(admin.ModelAdmin):
-    list_filter = ('professor',)
-    list_display = ('professor', 'day', 'even_week')
+@admin.register(models.Group)
+class Group(admin.ModelAdmin):
+    list_display = ('id', 'name', 'mail', 'id_pallada')
+    change_list_template = 'admin/groups_import.html'
 
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import/', self.import_all),
+        ]
+        return my_urls + urls
 
-@admin.register(models.Place)
-class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    def import_all(self, request):
+        Process(
+            target=setters.load_all_groups_from_pallada
+        ).start()
+        return HttpResponseRedirect("../")
 
-
-@admin.register(models.Subgroup)
-class SubgroupAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'get_professors', 'get_groups','subject', 'place', 'num')
-    list_filter = ('professors', 'groups', 'place',)
-    
-    def get_professors(self, obj):
-        return ", ".join([p.name for p in obj.professors.all()])
-
-    def get_groups(self, obj):
-        return ", ".join([p.name for p in obj.groups.all()])
 
 
 @admin.register(models.Lesson)
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ('time', 'get_subject')
+class Lesson(admin.ModelAdmin):
+    list_display = ('time', 'name', 'type', 'teacher', 'subgroup', 'place')
 
-    def get_subject(self, obj):
-        return ", ".join([s.subject.title for s in obj.subgroup.all()])
+    
