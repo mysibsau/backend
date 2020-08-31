5e44bec7c4dec5a0cc181fe74718137812847c3a
w0rng <antonabramov2000@gmail.com>
2020-08-31 15:29:24 +0700

    Хеш групп теперь в распесе (по требованию Малого)

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 import api.models as models
 
+from api.services import getters
+
 
 class GroupSerializers(serializers.ModelSerializer):
     class Meta:
@@ -34,7 +36,11 @@ class TimetableSerializers(serializers.Serializer):
     group = serializers.StringRelatedField(source='group.name')
     even_week = DaySerializers(many=True, read_only=True)
     odd_week = DaySerializers(many=True, read_only=True)
+    hash = serializers.SerializerMethodField('get_hash')
+
+    def get_hash(self, t):
+        return getters.get_hash()['hash']
 
     class Meta:
         model = models.TimetableGroup
-        fields = ('group', 'even_week', 'odd_week')
+        fields = ('group', 'hash', 'even_week', 'odd_week')
