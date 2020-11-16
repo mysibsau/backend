41de103d236ab2d91c6af250bba03ca74b74b7b8
w0rng <antonabramov2000@gmail.com>
2020-11-16 09:27:10 +0700

    Коментарии + описание типов

--- api/v2/services/getters.py
+++ api/v2/services/getters.py
@@ -6,52 +6,78 @@ from django.http import Http404
 from functools import lru_cache
 
 
-def get_all_groups_as_json():
+def get_all_groups_as_json() -> dict:
+    '''
+        Возвращает все группы, отфармотированные в слоарье
+    '''
     queryset = models.Group.objects.all()
     return serializers.GroupSerializers(queryset)
 
 
-def get_all_teachers_as_json():
+def get_all_teachers_as_json() -> dict:
+    '''
+        Возвращает всех преподавателей, отфармотированные в слоарье
+    '''
     queryset = models.Teacher.objects.all()
     return serializers.TeacherSerializers(queryset)
 
 
-def get_all_places_as_json():
+def get_all_places_as_json() -> dict:
+    '''
+        Возвращает все кабинеты, отфармотированные в слоарье
+    '''
     queryset = models.Place.objects.all()
     return serializers.PlaceSerializers(queryset)
 
 
-@lru_cache(maxsize=1024)
-def get_hash():
+def get_hash() -> dict:
+    '''
+        Генерирует хэш, основываясь на списке групп
+    '''
     return utils.generate_hash(str(get_all_groups_as_json()))
 
 
-def get_current_week_evenness_as_json():
+def get_current_week_evenness_as_json() -> dict:
+    '''
+        Возвращает номер недели
+    '''
     return {
         'week': utils.get_current_week_evenness()
     }
 
 
 @lru_cache(maxsize=1024)
-def get_timetable(obj_id):
-    queryset = models.Timetable.objects.filter(group__id=obj_id)
+def get_timetable(obj_id) -> dict:
+    '''
+        Возвращает расписание конкретной группы
+    '''
+    queryset = models.Timetable.objects.filter(group__id=obj_id).select_related()
     return serializers.TimetableSerializers(queryset)
 
 
 @lru_cache(maxsize=1024)
-def get_timetable_teacher(obj_id):
-    queryset = models.Timetable.objects.filter(teacher__id=obj_id)
+def get_timetable_teacher(obj_id) -> dict:
+    '''
+        Возвращает расписание преподавателя
+    '''
+    queryset = models.Timetable.objects.filter(teacher__id=obj_id).select_related()
     return serializers.TimetableSerializers(queryset)
 
 
-def get_meta():
+def get_meta() -> dict:
+    '''
+        Возвращает метаданные (номер недели и хэш)
+    '''
     return {
         'week': utils.get_current_week_evenness(),
         'hash': get_hash(),
     }
 
 @lru_cache(maxsize=1024)
-def select_day(queryset, day, week):
+def select_day(queryset, day: int, week: int) -> dict:
+    '''
+        Возвращает все пары, которые проходили в конкретный день
+    '''
     result = []
     for q in queryset :
         if q.day == day and q.week == week:
@@ -60,7 +86,11 @@ def select_day(queryset, day, week):
     return result
 
 
-def select_lessons(queryset, time):
+
+def select_lessons(queryset, time: str) -> dict:
+    '''
+        Возвращает все пары, которые проходили в конкретный час
+    '''
     result = []
     for q in queryset:
         if q.time == time:
