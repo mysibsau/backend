9237fb5fe4be9f91235934fecbf37f277c5fff72
w0rng <antonabramov2000@gmail.com>
2020-12-29 00:58:01 +0700

    проверки + запись ответов

--- apps/surveys/views.py
+++ apps/surveys/views.py
@@ -4,15 +4,41 @@ from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
 
 from apps.surveys import models, serializers
+from apps.surveys.services import setters, check
+import json
 
 
 class SurveysView(viewsets.ViewSet):
     @method_decorator(cache_page(60*60*2))
     def all(self, request):
+        uuid = request.GET.get('uuid')
+        if not uuid:
+            return Response('not uuid', 405)
         queryset = models.Survey.objects.all()
-        return Response(serializers.SurveysSeializers(queryset))
+        return Response(serializers.SurveysSeializers(queryset, uuid))
 
+    @method_decorator(cache_page(60*60*2))
     def one(self, request, obj_id):
+        uuid = request.GET.get('uuid')
+        if not uuid:
+            return Response('not uuid', 405)
+        if check.user_already_answered(uuid, obj_id):
+            return Response('Вы уже прогосовали', 405)
         queryset = models.Survey.objects.filter(
             id=obj_id).select_related().first()
         return Response(serializers.SurveySeializers(queryset))
+
+    def set_answer(self, request, obj_id):
+        data = dict()
+        try:
+            data = json.loads(request.body)
+        except:
+            return Response('Что-то сломалось', 405)
+
+        if 'uuid' not in data:
+            return Response('not uuid', 405)
+        if 'questions' not in data:
+            return Response('not questions', 405)
+        if check.user_already_answered(data['uuid'], obj_id):
+            return Response('uuid already answered', 405)
+        return setters.set_answers(data, obj_id)
