b0c1442bdf89fa18725a044512f954a53d5255a9
w0rng <antonabramov2000@gmail.com>
2020-12-29 02:31:09 +0700

    старая апишка также доступна по адресу v1/

--- server/urls.py
+++ server/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path('', include('apps.api.v1.urls')),
+    path('v1/', include('apps.api.v1.urls')),
     path('v2/', include('apps.api.v2.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
