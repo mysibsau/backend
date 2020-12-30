9663242ffd50958163276328fe1396ee37db2e47
w0rng <antonabramov2000@gmail.com>
2020-12-30 21:32:44 +0700

    Тесты для проверки типа ответов

--- tests/surveys/test_services.py
+++ tests/surveys/test_services.py
@@ -7,7 +7,7 @@ from apps.surveys.services import getters, check
 from datetime import timedelta
 
 
-class GettersTest(TestCase):
+class GettersGetAllTest(TestCase):
     def setUp(self):
         self.uuid = ''.join([
             choice(ascii_letters) for _ in range(randint(10, 100))
@@ -25,7 +25,7 @@ class GettersTest(TestCase):
         count = queryset.count()
         self.assertEqual(count, 3)
 
-    def test_user_respond(self):
+    def test_user_did_respond(self):
         """
         Проверка колличества опросов, если пользователь отвечал
         """
@@ -48,7 +48,7 @@ class GettersTest(TestCase):
         self.assertEqual(count, 2)
 
 
-class CheckTest(TestCase):
+class CheckAlreadyAnswerTest(TestCase):
     def setUp(self):
         self.uuid = ''.join([
             choice(ascii_letters) for _ in range(randint(10, 100))
@@ -56,14 +56,14 @@ class CheckTest(TestCase):
         date = timezone.localtime() + timedelta(1)
         models.Survey.objects.create(name='Test', date_to=date)
 
-    def test_user_did_not_respond(self):
+    def test_user_already_answered_if_user_did_not_respond(self):
         """
         Проверка, если пользователь ни на что не отвечал
         """
         result = check.user_already_answered(self.uuid, 1)
         self.assertEqual(result, False)
 
-    def test_user_respond(self):
+    def test_user_already_answered_if_user_did_respond(self):
         """
         Проверка, если пользователь отвечал
         """
@@ -82,3 +82,75 @@ class CheckTest(TestCase):
 
         result = check.user_already_answered(self.uuid, 1)
         self.assertEqual(result, True)
+
+
+class CheckTypeQuestion(TestCase):
+    def setUp(self):
+        survey = models.Survey.objects.create(name='1', date_to=timezone.now())
+        question_one_answer = models.Question.objects.create(
+            survey = survey,
+            text = '1',
+            type = 0,
+            necessarily = True
+        )
+        question_many_answer = models.Question.objects.create(
+            survey = survey,
+            text = '1',
+            type = 1,
+            necessarily = True
+        )
+        question_answer_is_text = models.Question.objects.create(
+            survey = survey,
+            text = '1',
+            type = 2,
+            necessarily = True
+        )
+
+    def test_if_question_not_contain_answers_filed(self):
+        """
+        Проверяет, содержит ли присланный json поле answer
+        """
+        json = {'id': 1, }
+        result = check.check_type_question(json)
+        self.assertEqual(result, False)
+
+    def test_if_question_contain_many_answers(self):
+        """
+        Проверяет, не содержит ли присланный json лишних ответов
+        """
+        json = {'id': 1, 'answers': [0, 1]}
+        result = check.check_type_question(json)
+        self.assertEqual(result, False)
+
+    def test_if_question_not_contain_text_filed(self):
+        """
+        Проверяет, содержит ли присланный json поле text
+        """
+        json = {'id': 3, }
+        result = check.check_type_question(json)
+        self.assertEqual(result, False)
+
+    def test_good_question_answer_with_one_answer(self):
+        """
+        Проверяет, верный формат для вопроса с 1 ответом
+        """
+        json = {'id': 1, 'answers': [0,]}
+        result = check.check_type_question(json)
+        self.assertEqual(result, True)
+
+    def test_good_question_answer_with_many_answer(self):
+        """
+        Проверяет, верный формат для вопроса с несколькими ответами
+        """
+        json = {'id': 2, 'answers': [0, 1]}
+        result = check.check_type_question(json)
+        self.assertEqual(result, True)
+
+    def test_good_question_answer_with_text_answer(self):
+        """
+        Проверяет, верный формат для вопроса с текстовым ответом
+        """
+        json = {'id': 3, 'text': '123'}
+        result = check.check_type_question(json)
+        self.assertEqual(result, True)
+
