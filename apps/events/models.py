37331e5cea641b296bc14b9a1a3da82da085590f
w0rng <antonabramov2000@gmail.com>
2021-01-01 14:11:37 +0700

    fix путь к сохранению изображений и опечатка в названии поля

--- apps/events/models.py
+++ apps/events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Event(models.Model):
     name = models.TextField('Название')
-    logo = models.ImageField(verbose_name='Афига', upload_to='static/campus_sibsau_photos/')
+    logo = models.ImageField(verbose_name='Афиша', upload_to='events_logo/')
     text = models.TextField('Текст поста')
     author = models.CharField(verbose_name='Заказчик', max_length=200)
 
@@ -18,7 +18,11 @@ class Event(models.Model):
 class Link(models.Model):
     name = models.TextField('название')
     link = models.URLField('Ссылка')
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
+    event = models.ForeignKey(
+        Event, 
+        on_delete = models.CASCADE, 
+        verbose_name = 'Мероприятие'
+    )
 
     def __str__(self):
         return self.name
