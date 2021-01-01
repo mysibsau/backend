17262c7f53c96e77927e276f4a1c7e38b60123af
w0rng <antonabramov2000@gmail.com>
2021-01-01 13:44:42 +0700

    чуть-чуть изменил структуру версий url'ов

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -100,9 +100,9 @@ if env.bool('SENTRY'):
     import sentry_sdk
 
     sentry_sdk.init(
-        dsn=env.str('DSN'),
-        integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
+        dsn = env.str('DSN'),
+        integrations = [DjangoIntegration()],
+        traces_sample_rate = 1.0,
 
-        send_default_pii=True
+        send_default_pii = True
     )
--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path('', include('apps.api.v1.urls')),
-    path('v1/', include('apps.api.v1.urls')),
-    path('v2/', include('apps.api.v2.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('apps.api.urls_v1')),
+    path('v1/', include('apps.api.urls_v1')),
+    path('v2/', include('apps.api.urls.v2')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Мой СибГУ'
