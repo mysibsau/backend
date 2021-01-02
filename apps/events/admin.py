a4eafc2936a7ae619c21f003297fd16115bf6486
w0rng <antonabramov2000@gmail.com>
2021-01-02 22:48:44 +0700

    Фильтрация у мероприятий

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -5,8 +5,10 @@ from apps.events import models
 @admin.register(models.Event)
 class Event(admin.ModelAdmin):
     list_display = ('id', 'name', 'logo', 'text', 'author')
+    list_filter = ('author', )
 
 
 @admin.register(models.Link)
 class Link(admin.ModelAdmin):
     list_display = ('id', 'name', 'link', 'event')
+    list_filter = ('event', )
