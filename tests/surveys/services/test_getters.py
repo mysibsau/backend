d95e8282f6fe3914dd3b3396aeeeb36ef892ec46
w0rng <antonabramov2000@gmail.com>
2021-01-02 22:39:56 +0700

    Тесты для опросов с повторяющимся ответом

--- tests/surveys/services/test_getters.py
+++ tests/surveys/services/test_getters.py
@@ -15,7 +15,7 @@ class GettersGetAllTest(TestCase):
         date = timezone.localtime() + timedelta(1)
         models.Survey.objects.create(name='Test', date_to=date)
         models.Survey.objects.create(name='Test2', date_to=date)
-        models.Survey.objects.create(name='Tes3', date_to=date)
+        models.Survey.objects.create(name='Tes3', date_to=date, reanswer=True)
 
     def test_user_did_not_respond(self):
         """
@@ -25,11 +25,21 @@ class GettersGetAllTest(TestCase):
         count = queryset.count()
         self.assertEqual(count, 3)
 
+    def test_count_tests_if_one_old(self):
+        """
+        Проверка колличества опросов, если один из них истек
+        """
+        date = timezone.localtime() - timedelta(1)
+        models.Survey.objects.create(name='Test2', date_to=date)
+        queryset = getters.get_all_surveys_for_uuid(self.uuid)
+        count = queryset.count()
+        self.assertEqual(count, 3)
+
     def test_user_did_respond(self):
         """
         Проверка колличества опросов, если пользователь отвечал
         """
-        index = randint(1, 3)
+        index = randint(1, 2)
         survey = models.Survey.objects.get(id=index)
         question = models.Question.objects.create(
             survey = survey,
@@ -47,4 +57,25 @@ class GettersGetAllTest(TestCase):
         count = queryset.count()
         self.assertEqual(count, 2)
 
+    def test_user_did_respond_on_reanswer_test(self):
+        """
+        Проверка колличества опросов, если пользователь отвечал на вопрос с
+        повторным ответом
+        """
+        survey = models.Survey.objects.get(id=3)
+        question = models.Question.objects.create(
+            survey = survey,
+            text = '1',
+            type = 0,
+            necessarily = False
+        )
+        models.Answer.objects.create(
+            who = self.uuid,
+            survey = survey,
+            question = question,
+        )
+
+        queryset = getters.get_all_surveys_for_uuid(self.uuid)
+        count = queryset.count()
+        self.assertEqual(count, 3)
 
