224c0d849a163edd0f0c0bd5824b9d383cbd7bbd
w0rng <antonabramov2000@gmail.com>
2021-01-02 22:57:57 +0700

    Удобное редаактирование вложенных ссылок

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from apps.events import models
 
 
+class LinkInline(admin.TabularInline):
+    model = models.Link
+    extra = 1
+
+
 @admin.register(models.Event)
 class Event(admin.ModelAdmin):
     list_display = ('id', 'name', 'logo', 'text', 'author')
     list_filter = ('author', )
+    inlines = [
+        LinkInline,
+    ]
 
 
 @admin.register(models.Link)
