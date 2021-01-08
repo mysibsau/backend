04c936117641e8e3bb748b7bd4eb938ca8286f41
w0rng <antonabramov2000@gmail.com>
2021-01-08 20:10:01 +0700

    Кэширование мероприятий

--- apps/events/api/views.py
+++ apps/events/api/views.py
@@ -4,10 +4,12 @@ from apps.events import models, logger
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
 from . import serializers, docs
+from django.views.decorators.cache import cache_page
 
 
 @swagger_auto_schema(**docs.swagger_all_events)
 @api_view(['GET'])
+@cache_page(60 * 60)
 def all_events(request):
     """
     Возвращает список всех мероприятий, дата которых еще не истекла
