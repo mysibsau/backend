a61c1dace41491f7e87ed03688bd131eaa4683d7
w0rng <antonabramov2000@gmail.com>
2021-01-08 13:28:01 +0700

    Удалил приложение со статистикой

--- apps/statistic/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from apps.statistic import models
-
-
-@admin.register(models.Statistics)
-class Statistics(admin.ModelAdmin):
-    list_display = ('id', 'url', 'count')
