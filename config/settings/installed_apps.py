c33a5c8ff4df349b705250bfff1cba9174375499
w0rng <antonabramov2000@gmail.com>
2021-01-08 11:34:04 +0700

    рефакторинг согласно falke8

--- config/settings/installed_apps.py
+++ config/settings/installed_apps.py
@@ -27,7 +27,7 @@ LOCAL_MIGRATIONS = [app_path.split('.')[1] for app_path in LOCAL_APPS]
 MIGRATION_PATH = 'config.migrations.'
 
 MIGRATION_MODULES = {
-    app_name: MIGRATION_PATH + app_name 
+    app_name: MIGRATION_PATH + app_name
     for app_name in LOCAL_MIGRATIONS
 }
 
