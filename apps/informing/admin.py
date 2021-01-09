15dc2b00a154d550a6115eb89432d0545204ad9f
w0rng <antonabramov2000@gmail.com>
2021-01-09 20:48:58 +0700

    Админочка для приложения

---
+++ apps/informing/admin.py
@@ -0,0 +1,42 @@
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+from django.contrib import admin
+from . import models
+
+
+class AbstractInformationAdmin():
+    """
+    Абстрактный класс, чтобы не повторяться с написанием логики сохранения моделей
+    и подсчета лайков
+    """
+    def save_model(self, request, obj, form, change):
+        obj.author = str(request.user)
+        obj.save()
+
+    def likes(self, obj):
+        return models.Like.objects.filter(information__id=obj.id).count()
+
+    likes.short_description = 'Лайки'
+
+
+class ImageAdmin(NestedStackedInline):
+    model = models.Image
+    extra = 0
+    fk_name = 'news'
+
+
+class LinkAdmin(NestedStackedInline):
+    model = models.Link
+    extra = 0
+    fk_name = 'information'
+
+
+@admin.register(models.Event)
+class EventAdmin(AbstractInformationAdmin, admin.ModelAdmin):
+    list_display = ('id', 'name', 'logo', 'author', 'date_to', 'views', 'likes')
+    inlines = [LinkAdmin, ]
+
+
+@admin.register(models.News)
+class NewsAdmin(AbstractInformationAdmin, NestedModelAdmin):
+    list_display = ('id', 'author', 'date_to', 'views', 'likes')
+    inlines = [ImageAdmin, LinkAdmin]
