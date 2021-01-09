2cf4fb65e7411759c07b36b5390b1e3564415797
w0rng <antonabramov2000@gmail.com>
2021-01-09 22:31:27 +0700

    Можно ставить и убирать лайки

--- apps/informing/api/urls.py
+++ apps/informing/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('all_events/', views.all_events),
     path('all_news/', views.all_news),
+    path('like/<int:news_id>/', views.like_news)
 ]
--- apps/informing/api/views.py
+++ apps/informing/api/views.py
@@ -2,13 +2,11 @@ from rest_framework.response import Response
 from django.utils import timezone
 from rest_framework.decorators import api_view
 from . import serializers
-from django.views.decorators.cache import cache_page
 from .. import models
-from ..services import getters
+from ..services import getters, setters
 
 
 @api_view(['GET'])
-@cache_page(60 * 60)
 def all_events(request):
     """
     Возвращает список всех мероприятий, дата которых еще не истекла
@@ -22,7 +20,6 @@ def all_events(request):
 
 
 @api_view(['GET'])
-@cache_page(60 * 60)
 def all_news(request):
     """
     Возвращает список всех новостей, дата которых еще не истекла
@@ -30,9 +27,24 @@ def all_news(request):
     uuid = request.GET.get('uuid')
     if not uuid:
         return Response({'error': 'not uuid'}, 401)
-    queryset = models.News.objects.filter(
-        date_to__gt=timezone.localtime()
-    ).select_related()
+    queryset = models.News.objects.filter(date_to__gt=timezone.localtime())
     liked = getters.get_ids_liked_information_for_uuid(uuid)
     print(liked)
     return Response(serializers.NewsSerializer(queryset, liked))
+
+
+@api_view(['GET'])
+def like_news(request, news_id):
+    uuid = request.GET.get('uuid')
+    if not uuid:
+        return Response({'error': 'not uuid'}, 401)
+
+    information = models.Information.objects.filter(
+        date_to__gt=timezone.localtime(),
+        id=news_id
+    ).first()
+
+    if not information:
+        return Response({'error': 'Запись не найдена'}, 404)
+
+    return Response(*setters.like_it(uuid, information))
