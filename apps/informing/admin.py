c508adc22e31600c616e626f6cdefdafa70d30f4
w0rng <antonabramov2000@gmail.com>
2021-01-12 00:09:41 +0700

    Админка новых моделек

--- apps/informing/admin.py
+++ apps/informing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from .services.send_notification import send_notification
 
 
 class AbstractInformationAdmin():
@@ -28,3 +29,32 @@ class EventAdmin(AbstractInformationAdmin, admin.ModelAdmin):
 class NewsAdmin(AbstractInformationAdmin, admin.ModelAdmin):
     list_display = ('id', 'author', 'date_to', 'views', 'likes')
     inlines = [ImageAdmin, ]
+
+
+@admin.register(models.Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'text', 'image', 'message_id', 'get_all_topics')
+    filter_horizontal = ('topics',)
+
+    def get_all_topics(self, obj):
+        return ', '.join([topic.name for topic in obj.topics.all()])
+
+    get_all_topics.short_description = "Топики"
+
+    def save_related(self, request, form, formsets, change):
+        form.save_m2m()
+        for formset in formsets:
+            self.save_formset(request, form, formset, change=change)
+        obj = form.instance
+        message_ids = send_notification(obj, context={"request": request})
+        obj.message_id = ', '.join(message_ids)
+        obj.save()
+
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+    def save_model(self, request, obj, form, change):
+        models.Topic.objects.create(name=f'{obj.name}_ios', description=obj.description)
+        models.Topic.objects.create(name=f'{obj.name}_android', description=obj.description)
