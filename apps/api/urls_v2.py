3fa1679531617098c9f135aefcf70c4ba3a3de42
w0rng <antonabramov2000@gmail.com>
2021-01-15 21:03:35 +0700

    удалил приложение events

--- apps/api/urls_v2.py
+++ apps/api/urls_v2.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('timetable/', include('apps.timetable.api.v2.urls')),
     path('campus/', include('apps.campus_sibsau.api.urls')),
-    path('events/', include('apps.events.api.urls')),
     path('informing/', include('apps.informing.api.urls')),
     path('surveys/', include('apps.surveys.api.urls'))
 ]
--- apps/events/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-default_app_config = 'apps.events.apps.EventsConfig'
