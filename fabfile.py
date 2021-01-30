eebb0ddfa2fd508c090fa8f43ce9a1b1c86d18b1
w0rng <antonabramov2000@gmail.com>
2021-01-30 17:10:24 +0700

    Создание бэкапов базы данных

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from fabric import task, Connection
 from os import getenv
 from time import time
+from invoke import Responder
 
 
 connect_kwargs = {'password': getenv('SERVER_PASSWORD')}
@@ -15,13 +16,34 @@ def _get_time():
     return int(time() * 1000)
 
 
+def _media_backup(c):
+    print('\tbackup media...')
+    c.run(f'tar -cf media_{_get_time()}.tar.gz {PATH}media')
+    print('\tOK media\n')
+
+
+def _db_backup(c):
+    print('\tbackup database...')
+    db_password = Responder(
+        pattern='Password:',
+        response=f'{getenv("DATABASE_PASSWORD")}\n',
+    )
+    c.run(
+        f'pg_dump -h 127.0.0.1 -U {getenv("DATABASE_USER")} -F t'
+        f' -f dump_db_{_get_time()}.tar {getenv("DATABASE_NAME")}',
+        watchers=[db_password]
+    )
+    print('\tOK database')
+
+
 @task
 def backup(c):
     """
     Создание резервных копий
     """
     print('Run backup...')
-    print('\tbackup media...')
-    server.run(f'tar -cf media_{_get_time()}.tar.gz {PATH}media')
-    print('\tOK media')
+
+    _media_backup(server)
+    _db_backup(server)
+
     print('OK')
