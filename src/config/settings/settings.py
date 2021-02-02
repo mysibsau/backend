f00bd19b3b58202db0147a8e534668973693409a
w0rng <antonabramov2000@gmail.com>
2021-02-02 13:06:58 +0700

    Вынес мидлвари из настроек

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'config.middlewares.set_language.set_language'
+    'middlewares.set_language.set_language'
 ]
 
 ##################################################################
