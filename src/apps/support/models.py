5d1da25e822f8b096a8220b1a66891efb3bcf1fe
w0rng <antonabramov2000@gmail.com>
2021-02-03 18:06:01 +0700

    Поле с ответом может быть пустым

--- src/apps/support/models.py
+++ src/apps/support/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class FAQ(models.Model):
     question = models.TextField('Вопрос')
-    answer = models.TextField('Ответ')
+    answer = models.TextField('Ответ', blank=True)
     views = models.PositiveIntegerField('Просмотры', editable=False, default=0)
 
     def __str__(self):
