4f14c6b7fc236e7ae227f366343841eff7686c67
w0rng <antonabramov2000@gmail.com>
2021-02-03 17:48:33 +0700

    Моделька для FAQ

---
+++ src/apps/support/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class FAQ(models.Model):
+    question = models.TextField('Вопрос')
+    answer = models.TextField('Ответ')
+    views = models.PositiveIntegerField('Просмотры', editable=False, default=0)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'FAQ'
+        verbose_name_plural = 'FAQ'
