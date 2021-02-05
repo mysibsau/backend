510b0defb79b8df7c841a70bf98bce02a4bde0b6
w0rng <antonabramov2000@gmail.com>
2021-02-05 21:39:46 +0700

    Некоторые поля могут быть пустыми

--- src/apps/work/models.py
+++ src/apps/work/models.py
@@ -5,12 +5,12 @@ from django.utils.translation import gettext as _
 class Vacancy(models.Model):
     name = models.CharField(_('Название вакансии'), max_length=128)
     company = models.CharField('Компания', max_length=128)
-    duties = models.TextField('Обязаности')
-    requirements = models.TextField('Требования')
+    duties = models.TextField('Обязаности', blank=True)
+    requirements = models.TextField('Требования', blank=True)
     conditions = models.TextField('Условия')
-    schedule = models.TextField('Условия работы')
+    schedule = models.TextField('График работы', blank=True)
     address = models.TextField('Адрес')
-    add_info = models.TextField('Дополнительная информация')
+    add_info = models.TextField('Дополнительная информация', blank=True)
     contacts = models.TextField('Контакты')
     publication_date = models.DateField('Дата публикации')
     hidden = models.BooleanField('Скрыть', default=False)
@@ -21,3 +21,4 @@ class Vacancy(models.Model):
     class Meta:
         verbose_name = 'Вакансия'
         verbose_name_plural = 'Вакансии'
+        ordering = ['-publication_date']
