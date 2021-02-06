f5d4d107f6b1ea7841a97cc5a095b436ec85ce9e
w0rng <antonabramov2000@gmail.com>
2021-02-06 11:06:03 +0700

    Добавил поле с зарплатой

--- src/apps/work/models.py
+++ src/apps/work/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.utils.translation import gettext as _
 
 
 class Vacancy(models.Model):
-    name = models.CharField(_('Название вакансии'), max_length=128)
+    name = models.CharField('Название вакансии', max_length=128)
     company = models.CharField('Компания', max_length=128)
     duties = models.TextField('Обязаности', blank=True)
     requirements = models.TextField('Требования', blank=True)
     conditions = models.TextField('Условия')
     schedule = models.TextField('График работы', blank=True)
+    salary = models.TextField('Зарплата', blank=True)
     address = models.TextField('Адрес')
     add_info = models.TextField('Дополнительная информация', blank=True)
     contacts = models.TextField('Контакты')
