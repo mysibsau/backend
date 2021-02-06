283a6c8a96391649621be9857a792a17ad099ea5
w0rng <antonabramov2000@gmail.com>
2021-02-06 11:06:21 +0700

    Не парсить мусорные вакансии

--- src/apps/work/services/parsers.py
+++ src/apps/work/services/parsers.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from pprint import pprint
+from datetime import datetime
 
 
 def get_vacancy_list(page):
@@ -17,49 +17,40 @@ def get_name_vacancy(vacancy):
     return parse_name(vacancy.find('a').text)
 
 
-def get_list_from_ul(ul):
-    return [li.text for li in ul.find_all('li')]
-
-
 def get_string_field(vacancy, num_field):
     return parse_name(vacancy.find_all('tr')[num_field].find('td').text)
 
 
-def get_list_field(vacancy, num_field):
-    return get_list_from_ul(vacancy.find_all('tr')[num_field].find('td'))
+def is_valid_vacancy(vacancy):
+    for key in ['address', 'contacts', 'conditions']:
+        if len(vacancy[key]) <= 2:
+            return False
+    return True
 
 
-def get_vacancyes_info():
+def get_vacancies():
     response = requests.get(
         'https://www.sibsau.ru/page/graduate-vacancy'
     )
 
+    if response.status_code != 200:
+        return 'Error'
+
     soup = BeautifulSoup(response.text, 'html.parser')
 
     for vacancy in get_vacancy_list(soup):
-        yield {
+        vac = {
             'name': get_name_vacancy(vacancy),
             'company': get_string_field(vacancy, 0),
-            'duties': get_list_field(vacancy, 1),
-            'requirements': get_list_field(vacancy, 2),
-            'conditions': get_list_field(vacancy, 3),
-            'work_shedule': get_string_field(vacancy, 4),
+            'duties': get_string_field(vacancy, 1),
+            'requirements': get_string_field(vacancy, 2),
+            'conditions': get_string_field(vacancy, 3),
+            'schedule': get_string_field(vacancy, 4),
             'salary': get_string_field(vacancy, 5),
-            'adrees': get_string_field(vacancy, 6),
+            'address': get_string_field(vacancy, 6),
             'add_info': get_string_field(vacancy, 7),
-            'contacs': get_string_field(vacancy, 8),
-            'publicated_date': get_string_field(vacancy, 9),
+            'contacts': get_string_field(vacancy, 8),
+            'publication_date': datetime.strptime(get_string_field(vacancy, 9), '%d.%m.%Y'),
         }
-
-
-def main():
-    for vacancy in get_vacancyes_info():
-        print(vacancy['name'])
-        if len(vacancy['contacs']) <= 3:
-            continue
-        pprint(vacancy['contacs'])
-        print('-------------------')
-
-
-if __name__ == '__main__':
-    main()
+        if is_valid_vacancy(vac):
+            yield vac
