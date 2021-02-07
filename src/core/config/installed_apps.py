d05496fcf5a46c31411127b1aa1f7499ab30c241
w0rng <antonabramov2000@gmail.com>
2021-02-07 19:25:13 +0700

    Вынес миграции в каталоги приложений

--- src/core/config/installed_apps.py
+++ src/core/config/installed_apps.py
@@ -16,12 +16,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'constance',
     'constance.backends.database',
-]
 
-if settings.DEBUG:
-    INSTALLED_APPS.append('debug_toolbar')
-
-LOCAL_APPS = [
     'apps.timetable',
     'apps.campus_sibsau',
     'apps.surveys',
@@ -31,18 +26,5 @@ LOCAL_APPS = [
     'apps.shop',
 ]
 
-INSTALLED_APPS += LOCAL_APPS
-
-LOCAL_MIGRATIONS = [app_path.split('.')[1] for app_path in LOCAL_APPS]
-
-MIGRATION_PATH = 'core.migrations.'
-
-MIGRATION_MODULES = {
-    app_name: MIGRATION_PATH + app_name
-    for app_name in LOCAL_MIGRATIONS
-}
-
-for app in LOCAL_MIGRATIONS:
-    if not path.exists(f'core/migrations/{app}'):
-        mkdir(f'core/migrations/{app}')
-        open(f'core/migrations/{app}/__init__.py', 'w+').close()
+if settings.DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
