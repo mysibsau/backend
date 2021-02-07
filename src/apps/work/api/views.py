bf33eafd0c791b943eb3cedc3a2ccd47d0dda0d9
w0rng <antonabramov2000@gmail.com>
2021-02-07 12:02:50 +0700

    fix документация для вакансий

--- src/apps/work/api/views.py
+++ src/apps/work/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
+from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
 from apps.work import models
 from apps.work.api import serializers, docs
@@ -7,6 +8,7 @@ from apps.work.api import serializers, docs
 
 @swagger_auto_schema(**docs.swagger_all_vacancies)
 @api_view(['GET'])
+@cache_page(60 * 60)
 def all_vacancies(request):
     """
     Возвращает все вакансий
