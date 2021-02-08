60fbe7936814d214f96156867c68e7a6deb7619b
w0rng <antonabramov2000@gmail.com>
2021-02-08 21:29:29 +0700

    Парсинг расписания перевел на API

--- src/apps/user/services/test.py
+++ src/apps/user/services/test.py
@@ -2,33 +2,17 @@ import xmlrpc.client
 from pprint import pprint
 from time import time
 from apps.timetable.models import Group
+from api_pallada import API
 
 
-groups = Group.objects.all()[300:800]
-ids = [group.id_pallada for group in groups]
+ids = [group.id_pallada for group in Group.objects.all()][300:400]
+api = API('timetable')
 
-
-url = 'https://timetable.pallada.sibsau.ru'
-db = 'timetable'
-username = 'skorobogatov_ia'
-password = '847wmtgWMT'
-
-
-common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
-uid = common.authenticate(db, username, password, {})
-start = time()
-models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
-
-
-all_timetable_id = models.execute_kw(
-    db,
-    uid,
-    password,
-    'info.groups',
-    'read',
-    [ids],
-    {
-        'fields': ['tt_current_year_second_ids']
+all_timetable_id = api.read(
+    model_name='info.groups',
+    args=[ids],
+    kwargs={
+        'fields': ['tt_current_year_second_ids'],
     },
 )
 
@@ -38,12 +22,8 @@ for i in all_timetable_id:
     tmp += i['tt_current_year_second_ids']
 
 
-timetable = models.execute_kw(
-    db,
-    uid,
-    password,
+timetable = api.read(
     'info.timetable',
-    'read',
     [tmp],
     {
         'fields': ['day_week', 'employee_name_init', 'lesson', 'lesson_type_id', 'place', 'time']
@@ -51,5 +31,3 @@ timetable = models.execute_kw(
 )
 
 pprint(len(timetable))
-
-print(time() - start)
