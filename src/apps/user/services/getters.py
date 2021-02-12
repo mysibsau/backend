a3587756246d1365a17a54b895a10bdf55a4cd39
w0rng <antonabramov2000@gmail.com>
2021-02-12 11:45:47 +0700

    Получение информации о студенте

---
+++ src/apps/user/services/getters.py
@@ -0,0 +1,28 @@
+from api_pallada import API
+
+
+def get_gradebook(api: API):
+    return api.search_read(
+        'portfolio_science.grade_attistation_view',
+        [[['nzkn', '!=', '']]],
+        {'limit': 1},
+    )[0]['nzkn']
+
+
+def get_fio_group_and_average(api: API):
+    tmp = api.search_read(
+        'portfolio_science.grade_view',
+        [[['ID_student', '!=', '']]],
+        {'fields': ['ID_student', 'display_name', 'grade']},
+    )
+
+    average = 0
+    count = 0
+
+    for i in tmp:
+        if not i['grade'][0].isdigit():
+            continue
+        average += int(i['grade'][0])
+        count += 1
+
+    return tmp[0]['ID_student'], tmp[0]['display_name'], average / count
