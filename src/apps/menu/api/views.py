da755f95bc3ebe8f1a284ad08a4cd03ed968029c
w0rng <antonabramov2000@gmail.com>
2021-02-23 14:33:42 +0700

    рефакторинг вьюхи

--- src/apps/menu/api/views.py
+++ src/apps/menu/api/views.py
@@ -2,11 +2,10 @@ from rest_framework.response import Response
 from django.views.decorators.cache import cache_page
 from rest_framework.decorators import api_view
 from apps.menu.api.serializers import menu_serializers
-from apps.menu.models import Menu
+from apps.menu.services.getters import get_menu
 
 
 @api_view(['GET'])
 @cache_page(60 * 60)
 def all_menu(request):
-    queryset = Menu.objects.all()
-    return Response(menu_serializers(queryset), 200)
+    return Response(menu_serializers(get_menu()), 200)
