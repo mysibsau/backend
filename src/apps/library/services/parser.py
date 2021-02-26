9aecb723a29a38ee20eb323f74b316cc73134d26
Anton Abramov <antonabramov2000@gmail.com>
2021-02-26 23:08:36 +0700

    Library fix (#14)
    * добавил получение хранилища и количества книг
    
    * Подключил парсер кабинетов и колиечества
    
    * Очистка запроса от мусора
    
    * Рефакторинг
    
    * Типизация
    
    Co-authored-by: kiri11_mi1 <kirill.mil.0310@mail.ru>

--- src/apps/library/services/parser.py
+++ src/apps/library/services/parser.py
@@ -1,67 +1,77 @@
 from bs4 import BeautifulSoup
-from random import randint
 
 
 def get_book_quantities(soup):
-    """Получение книг на странице"""
-    return len(soup.find_all('input', {'name': "MFN"}))
+    '''Получение книг на странице'''
+    return len(soup.find_all('input', {'name': 'MFN'}))
 
 
-def get_parent(soup):
-    return soup.find_all('input', {'name': "MFN"})[0].parent.parent.text
+def __books(soup: BeautifulSoup) -> BeautifulSoup:
+    for num in range(get_book_quantities(soup)):
+        yield soup.find_all('input', {'name': 'MFN'})[num].parent.parent
 
 
-def get_text(soup, num):
-    """Получение текста под названием книги"""
-    return soup.find_all('input', {'name': "MFN"})[num].parent.parent.text
-
-
-def get_name_book(soup, num):
-    return get_text(soup, num)\
+def get_name_book(book: BeautifulSoup):
+    return book.text\
         .split('/')[0]\
         .split('\xa0\xa0\xa0\xa0')[-1]\
-        .split(': пер')[0]\
-        .split('[Электронный ресурс]')[0]\
-        .split('[Electronic resource]')[0]\
         .split(':')[0].strip()
 
 
-def get_link(soup, num):
-    if link := soup.find_all('input', {'name': "MFN"})[num].parent.parent.find_all('a', {'target': '_blank'}):
+def get_place_and_count(book):
+    '''Получение количества книг в хранилище'''
+    text = book.text
+
+    places = {
+        'УА': 'Л-208',
+        'СЭА': 'Л-203',
+        'НА': 'Л-204',
+        'ХА': 'Л-208',
+        'ХР': 'Л-208',
+    }
+
+    if 'отделах:' not in text:
+        return None, None
+    count = text.split('отделах: ')[-1].split(')')[0].split('(')[1]
+    count = int(count) if count.isdigit() else 0
+    place = text.split('отделах: ')[-1].split(')')[0].split('(')[0]
+    place = places.get(place, place)
+
+    return place, count
+
+
+def get_link(book: BeautifulSoup):
+    if link := book.find_all('a', {'target': '_blank'}):
         return link[0]['href']
-    else:
-        return None
 
 
-def get_author_name(soup, num):
-    if name := soup.find_all('input', {'name': "MFN"})[num].parent.parent.find_all('a', {'class':"term_hyper"}):
+def get_author_name(book: BeautifulSoup):
+    if name := book.find_all('a', {'class': 'term_hyper'}):
         return name[0].text
-    elif name := soup.find_all('input', {'name': "MFN"})[num].parent.parent.select('b:nth-child(5)'):
+    elif name := book.select('b:nth-child(5)'):
         return name[0].text
-    else:
-        return None
 
 
 def get_all_books(html):
     soup = BeautifulSoup(html, 'html.parser')
     result = {'digital': [], 'physical': []}
-    for num in range(get_book_quantities(soup)):
+    for book in __books(soup):
+        author = get_author_name(book)
+        name = get_name_book(book)
+        place, count = get_place_and_count(book)
+
+        if not all((author, name, place, count)):
+            continue
+
         result['physical'].append({
-            'author': get_author_name(soup, num),
-            'name': get_name_book(soup, num),
-            'place': f'Л {randint(1, 100)}',
-            'count': randint(0, 50),
+            'author': author,
+            'name': name,
+            'place': place,
+            'count': count,
         })
         result['digital'].append({
-            'author': get_author_name(soup, num),
-            'name': get_name_book(soup, num),
+            'author': author,
+            'name': name,
             'url': 'https://t.me/w0rng',
         })
     return result
-
-
-if __name__ == '__main__':
-    for book in get_all_books('IZDV.html'):
-        for key, value in book.items():
-            print(key, ":", value)
-        print()
