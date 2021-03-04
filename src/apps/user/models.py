c84cc75fb4318eb5a0df0a0be17f032711fb70a9
w0rng <antonabramov2000@gmail.com>
2021-03-04 09:28:46 +0700

    Добавил поле c фио в модель пользователя

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -5,13 +5,14 @@ from django.utils import timezone
 class User(models.Model):
     token = models.CharField('Токен', editable=False, max_length=16)
     group = models.CharField('Группа', max_length=16)
+    name = models.CharField('ФИО', max_length=128, blank=True)
     average = models.FloatField('Средний балл', default=5.0)
     creation_date = models.DateField('Дата регистрации', default=timezone.now)
     last_entry = models.DateTimeField('Последняя авторизация')
     banned = models.BooleanField('Заблокирован', default=False)
 
     def __str__(self):
-        return self.token[:4]
+        return f'{self.name} {self.group}'
 
     class Meta:
         pass
