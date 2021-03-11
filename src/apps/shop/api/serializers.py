b0ccbf6dd9171c191120011366abc5fb49d8304d
w0rng <antonabramov2000@gmail.com>
2021-03-11 16:53:44 +0700

    typing

--- src/apps/shop/api/serializers.py
+++ src/apps/shop/api/serializers.py
@@ -1,7 +1,8 @@
-from .. import models
+from typing import List
+from apps.shop.models import Purchase
 
 
-def UserTicketsSerializer(purchases):
+def UserTicketsSerializer(purchases: List[Purchase]) -> List[dict]:
     result = []
     for purchase in purchases:
         result.append({
@@ -11,6 +12,6 @@ def UserTicketsSerializer(purchases):
             'count': purchase.count,
             'datetime': purchase.datetime,
             'code': purchase.code,
-            'status': purchase.status
+            'status': purchase.status,
         })
     return result
