ad9b5cd9450c6c55e0439af13f3e7b8795036d1a
w0rng <antonabramov2000@gmail.com>
2021-03-20 16:14:43 +0700

    Покупка билетов

--- src/apps/tickets/api/views.py
+++ src/apps/tickets/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from datetime import timedelta
 from django.utils import timezone
+from json import loads as json_loads
+from apps.tickets.services import check_tickets
 
 
 @api_view(['GET'])
@@ -19,6 +21,9 @@ def user_ticket(request):
     if not user:
         return Response({'error': 'user not found'}, status=404)
 
+    if user.banned:
+        return Response({'error': 'you are banned'}, 405)
+
     queryset = models.Purchase.objects.filter(buyer=user)
 
     return Response(serializers.TicketsSerializer(queryset))
@@ -32,3 +37,32 @@ def all_perfomances(request):
     queryset = set(concert.performance for concert in queryset)
 
     return Response(serializers.PerfomancesSerializer(queryset))
+
+
+@api_view(['POST'])
+def buy(request):
+    token = request.GET.get('token')
+
+    if not token:
+        return Response({'error': 'not token'}, status=400)
+
+    user = User.objects.filter(token=token).first()
+    if not user:
+        return Response({'error': 'user not found'}, status=404)
+
+    if user.banned:
+        return Response({'error': 'you are banned'}, 405)
+
+    if not request.body:
+        return Response({'error': 'Не передан JSON'}, 400)
+
+    data = json_loads(request.body)
+    if not data.get('tickets'):
+        return Response({'error': 'Не переданы билеты для бронирования'}, 401)
+
+    queryset = models.Ticket.objects.filter(id__in=data['tickets'])
+
+    if chech_error_code := check_tickets.check_all_tickets(queryset):
+        return Response(chech_error_code, 405)
+
+    return Response(serializers.TicketSerializers(queryset))
