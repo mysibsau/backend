42835afe005ce0b4e6e67e2af3036774deef16b6
w0rng <antonabramov2000@gmail.com>
2021-03-20 16:14:52 +0700

    url для покупки билетов

--- src/apps/tickets/api/urls.py
+++ src/apps/tickets/api/urls.py
@@ -5,4 +5,5 @@ from apps.tickets.api import views
 urlpatterns = [
     path('my_tickets/', views.user_ticket),
     path('all_perfomances/', views.all_perfomances),
+    path('buy/', views.buy),
 ]
