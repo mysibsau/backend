44263f35e1ed82016648848981ace443c38c3440
w0rng <antonabramov2000@gmail.com>
2021-03-23 14:29:40 +0700

    Короткие названия столовых

--- src/apps/menu/models.py
+++ src/apps/menu/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 class DiningRoom(models.Model):
     name = models.CharField('Название', max_length=256)
+    short_name = models.CharField('Короткое название', max_length=128, blank=True)
 
     def __str__(self):
         return self.name
