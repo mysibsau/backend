cddbfa125980b56ab40549870b1dc47d4ee35b76
w0rng <antonabramov2000@gmail.com>
2021-03-25 10:55:43 +0700

    fix markdown links

--- src/apps/informing/services/utils.py
+++ src/apps/informing/services/utils.py
@@ -3,6 +3,6 @@ from re import sub
 
 def replace_vk_links_on_markdown(text: str) -> str:
     mask = r'(\[((id|club)\d+)\|([a-zA-Zа-яА-Я0-9 ]+)\])'
-    repl = r'(https://vk.com/\2)[\4]'
+    repl = r'[\4](https://vk.com/\2)'
 
     return sub(mask, repl, text)
