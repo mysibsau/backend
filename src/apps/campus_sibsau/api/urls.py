ce9b0b19da67e89cc12cbf1a90902c0c799d7552
w0rng <antonabramov2000@gmail.com>
2021-08-05 22:40:15 +0700

    рефакторинг сериалайзеров кампуса

--- src/apps/campus_sibsau/api/urls.py
+++ src/apps/campus_sibsau/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+
 from apps.campus_sibsau.api import views
 
 
 urlpatterns = [
-    path('unions/', views.all_unions),
+    path('unions/', views.UnionAPIView.as_view()),
     path('unions/join/<int:union_id>/', views.join_to_union),
-    path('institutes/', views.all_institutes),
-    path('buildings/', views.all_buildings),
-    path('sport_clubs/', views.all_sport_clubs),
-    path('design_offices/', views.all_design_office),
+    path('institutes/', views.InstituteAPIView.as_view()),
+    path('buildings/', views.BuildingAPIView.as_view()),
+    path('sport_clubs/', views.SportClubsAPIView.as_view()),
+    path('design_offices/', views.DesignOfficeAPIView.as_view()),
     path('ensembles/', views.EnsembleApiView.as_view()),
 ]
