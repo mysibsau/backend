36611f41f06eafcc9562ebd74b89d209a33281f9
kiri11-mi1 <kirill.mil.0310@mail.ru>
2021-08-09 13:01:27 +0700

    #96 добавил барьер токен в заголовках

--- src/middlewares/auth.py
+++ src/middlewares/auth.py
@@ -4,9 +4,16 @@ from rest_framework.response import Response
 
 def auth(get_response):
     def middleware(request):
-        user_token = request.GET.get("token")
+        user_token = request.headers.get("Authorization")
+
+        request.student = None
 
         if not user_token:
+            return get_response(request)
+
+        token_type, token = user_token.split()
+
+        if token_type != 'Bearer':
             return get_response(request)
 
         user = User.objects.filter(token=user_token).first()
