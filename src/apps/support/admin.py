efaae8263700b299a3b1a59027d48905f55987c4
w0rng <antonabramov2000@gmail.com>
2021-08-15 15:52:17 +0700

    #MYSIBGU-52 показывать вопросы только с нужной темой

--- src/apps/support/admin.py
+++ src/apps/support/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from apps.support import models
 from django.db.models import Q
 from modeltranslation.admin import TabbedTranslationAdmin
+
+from apps.support import models
 
 
 class BlankAnswerFilter(admin.SimpleListFilter):
@@ -28,6 +29,18 @@ class FAQAdmin(TabbedTranslationAdmin):
     list_display = ('id', 'question', 'theme', 'answer', 'views')
     list_filter = [BlankAnswerFilter]
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        permissions_and_slug = [(f'support.can_view_{theme.slug}', theme.slug) for theme in models.Theme.objects.all()]
+        user = request.user
+        if user.is_superuser:
+            return qs
+        result = models.FAQ.objects.none()
+        for permission, slug in permissions_and_slug:
+            if not user.has_perm(permission):
+                continue
+            result |= qs.filter(theme__slug=slug)
+        return result
 
 @admin.register(models.Theme)
 class ThemeAdmin(TabbedTranslationAdmin):
