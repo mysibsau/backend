6234f9926886751d83877c44476e677309ba9f2c
Kirill <kirill.mil.0310@mail.ru>
2021-08-15 18:30:35 +0700

    косяк с пользователями №37

--- src/apps/support/models.py
+++ src/apps/support/models.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.models import User, Permission
-from django.contrib.contenttypes.models import ContentType
+from apps.user.models import User
 from django.db import models
 
 
