b5b475d8f69ae755d07c4d7b85568b39962fb8b8
w0rng <antonabramov2000@gmail.com>
2021-08-15 12:03:58 +0700

    #MYSIBGU-52 добавил модель темы

--- src/apps/support/models.py
+++ src/apps/support/models.py
@@ -2,11 +2,32 @@ from django.db import models
 from apps.user.models import User
 
 
+class Theme(models.Model):
+    slug = models.CharField('Код темы', max_length=7, unique=True, primary_key=True)
+    title = models.CharField('Название темы', max_length=31)
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def get_default_id(cls) -> int:
+        obj, created = cls.objects.get_or_create(slug='default')
+        if created:
+            obj.title = 'Без темы'
+            obj.save()
+        return obj.pk
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+
+
 class FAQ(models.Model):
     question = models.TextField('Вопрос')
     answer = models.TextField('Ответ', blank=True)
     views = models.PositiveIntegerField('Просмотры', editable=False, default=0)
     user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
+    theme = models.ForeignKey(Theme, on_delete=models.SET_DEFAULT, default=Theme.get_default_id)
 
     def __str__(self):
         return self.question
