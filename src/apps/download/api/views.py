7edd7cb1a4b73ffab756a52d56806aa9c5e9ea85
w0rng <antonabramov2000@gmail.com>
2021-08-18 09:17:25 +0700

    Рендеринг страницы скачивания

--- src/apps/download/api/views.py
+++ src/apps/download/api/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from user_agents import parse
 from django.shortcuts import redirect
+from django.shortcuts import render
 
 
 def download(request):
@@ -11,4 +12,4 @@ def download(request):
         return redirect('https://play.google.com/store/apps/details?id=com.SibSU.MySibSU')
     if family == 'iOS':
         return redirect('https://apps.apple.com/ru/app/%D0%BC%D0%BE%D0%B9-%D1%81%D0%B8%D0%B1%D0%B3%D1%83/id1531466252')
-    raise Http404('Страница еще не готова')
+    return render(request, 'download/index.html')
