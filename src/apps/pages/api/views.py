5350343c6b1a6712e79c7f88b00ea643778f30c8
w0rng <me@w0rng.ru>
2021-08-22 10:36:47 +0700

    #MYSIBGU-99 fix name templates in view

--- src/apps/pages/api/views.py
+++ src/apps/pages/api/views.py
@@ -11,8 +11,8 @@ def download(request):
         return redirect('https://play.google.com/store/apps/details?id=com.SibSU.MySibSU')
     if family == 'iOS':
         return redirect('https://apps.apple.com/ru/app/%D0%BC%D0%BE%D0%B9-%D1%81%D0%B8%D0%B1%D0%B3%D1%83/id1531466252')
-    return render(request, 'download/index.html')
+    return render(request, 'pages/download/index.html')
 
 
 def user_agreement(request):
-    return render(request, 'user_agreement')
+    return render(request, 'pages/user_agreement/index.html')
