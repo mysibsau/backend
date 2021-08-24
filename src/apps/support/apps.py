57d9d98896d6a0232885fd14d3872a8bba2d32dc
Anton Abramov <me@w0rng.ru>
2021-08-25 00:11:52 +0700

    #MYSIBGU-95 Регисстрация сигнала

--- src/apps/support/apps.py
+++ src/apps/support/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class SupportConfig(AppConfig):
     name = 'apps.support'
     verbose_name = 'Помощь'
+
+    def ready(self) -> None:
+        from . import models
+        from apps.notifications.services.notifications_proscessor import Listener
+
+        Listener(model=models.FAQ)
