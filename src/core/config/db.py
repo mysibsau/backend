d0b943b81f33558f0502e134ee50bd0084caea8c
Anton Abramov <me@w0rng.ru>
2021-08-26 09:55:36 +0700

    #Docker настрил базу для докера

--- src/core/config/db.py
+++ src/core/config/db.py
@@ -1,22 +1,16 @@
-from sys import argv
-from core.config.environ import env
+import os
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'  # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
 
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env.str('DATABASE_NAME'),
-        'USER': env.str('DATABASE_USER'),
-        'PASSWORD': env.str('DATABASE_PASSWORD'),
-        'HOST': env.str('DATABASE_HOST'),
-        'PORT': env.str('DATABASE_PORT'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'database',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'PORT': 5432,
+    },
 }
-
-if len(argv) > 1 and argv[1] == 'test':
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
