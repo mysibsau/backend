09a76549f5c92d62ef16d8ba7902c5fc08062f32
Anton Abramov <me@w0rng.ru>
2021-08-26 21:48:50 +0700

    #dev удалил старые адреса админки и документации

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,27 +4,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-from drf_yasg import openapi
-
-
 admin.site.site_header = 'Мой СибГУ'
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title=f"API {admin.site.site_header}",
-        default_version='v2.1',
-        license=openapi.License(name="GNU General Public License v3.0"),
-    ),
-    public=True,
-    permission_classes=(permissions.IsAdminUser,),
-)
-
-
 urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
     path('v2/timetable/', include('apps.timetable.api.v2.urls')),
     path('v1/timetable/', include('apps.timetable.api.v1.urls')),
     path('', include('apps.timetable.api.v1.urls')),
@@ -40,7 +23,6 @@ urlpatterns = [
     path('v2/menu/', include('apps.menu.api.urls')),
     path('v2/library/', include('apps.library.api.urls')),
     path('v2/tickets/', include('apps.tickets.api.urls')),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0)),
     path('healthchecks/', include('django_healthchecks.urls')),
 ]
 
