d8c49359b41a69dab5a6e6c6cde9e788c13fd898
Anton Abramov <me@w0rng.ru>
2021-08-28 04:14:40 +0700

    #dev Настройки БД

--- src/core/settings/db.py
+++ src/core/settings/db.py
@@ -1,4 +1,4 @@
-import os
+from core.settings import env
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'  # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
@@ -8,9 +8,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': env.str('POSTGRES_USER', 'postgres'),
         'HOST': 'database',
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD', 'postgres'),
         'PORT': 5432,
     },
 }
