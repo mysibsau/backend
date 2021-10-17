3e11fe0cb9284e493c3e11186143299d8e352142
Anton Abramov <antonabramov2000@gmail.com>
2021-10-17 15:22:18 +0700

    кстомная валидация url

--- src/api/v3/campus_sibsau/serializers.py
+++ src/api/v3/campus_sibsau/serializers.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ValidationError
 from rest_framework import fields
+from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer, Serializer
+from django.core.validators import URLValidator
 
 from apps.campus_sibsau import models
 
@@ -32,6 +35,16 @@ class JoinFacultySerializer(Serializer):
     fio = fields.CharField(help_text='ФИО')
     institute = fields.CharField(help_text='Институт')
     group = fields.CharField(help_text='Группа')
-    vk = fields.URLField(help_text='Ссылка на вк')
+    vk = fields.SerializerMethodField(help_text='Ссылка на вк')
     hobby = fields.CharField(help_text='Увлечения')
     reason = fields.CharField(help_text='Причина на вступление')
+
+    def get_vk(self, *args):
+        vk_link = self.initial_data.get('vk')
+        if vk_link[:8] != 'https://':
+            vk_link = 'https://' + vk_link
+            self.initial_data['vk'] = vk_link
+
+        URLValidator()(vk_link)
+
+        return vk_link
