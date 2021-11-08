7d62237186901e55de15cc9b42b51fb377e006ba
Kirill <kirill.mil.0310@mail.ru>
2021-11-08 19:09:51 +0700

    #129 - добавил роутинг приложения расписания

--- src/api/v3/urls.py
+++ src/api/v3/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('campus/', include('api.v3.campus_sibsau.urls')),
     path('support/', include('api.v3.support.urls')),
+    path('timetable/', include('api.v3.timetable.urls')),
 ]
